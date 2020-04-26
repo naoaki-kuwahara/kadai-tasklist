c74395637702e8e39f9d26f7961fe4affa3d7b29
naoaki.kuwahara <ec2-user@ip-172-31-93-197.ec2.internal>
2020-04-26 07:09:53 +0000

    kadai_login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
  before_action :set_task, only:[:show, :edit, :update, :destroy]
- 
+ before_action :require_user_logged_in
  
   def index
   @tasks = Task.all
+  @tasks = current_user.tasks.order(id: :desc)
   end
 
   def show
@@ -15,11 +16,12 @@ class TasksController < ApplicationController
 
   def create
   @task = Task.new(task_params)
-
+  @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc)
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -53,9 +55,14 @@ class TasksController < ApplicationController
 
  def task_params
   params.require(:task).permit(:content , :status)
-
  end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
 
 
